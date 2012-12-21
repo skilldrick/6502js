dfb3af23be37ed476c551a520ebc0d2faa005483
BigEd <ed.spittles@gmail.com>
2012-12-21 10:27:01 +0000

    fixup dissassembly - off by one showed up when final instruction is single byte

--- assembler.js
+++ assembler.js
@@ -2429,7 +2429,7 @@ function SimulatorWidget(node) {
       var byte;
       var modeAndCode;
 
-      while (currentAddress < endAddress - 1) {
+      while (currentAddress < endAddress) {
         inst = createInstruction(currentAddress);
         byte = memory.get(currentAddress);
         inst.addByte(byte);
